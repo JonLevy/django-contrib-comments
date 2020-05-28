ebaacf9f1947773dd849f1927c4907830382dfa3
Jonathan Levy <jonl@advocatetax.com>
2020-05-28 15:43:59 -0400

    Disable unnecessary endpoints.

--- django_comments/urls.py
+++ django_comments/urls.py
@@ -8,14 +8,17 @@ from .views.moderation import (
 
 
 urlpatterns = [
-    url(r'^post/$', post_comment, name='comments-post-comment'),
-    url(r'^posted/$', comment_done, name='comments-comment-done'),
-    url(r'^flag/(\d+)/$', flag, name='comments-flag'),
-    url(r'^flagged/$', flag_done, name='comments-flag-done'),
-    url(r'^delete/(\d+)/$', delete, name='comments-delete'),
-    url(r'^deleted/$', delete_done, name='comments-delete-done'),
-    url(r'^approve/(\d+)/$', approve, name='comments-approve'),
-    url(r'^approved/$', approve_done, name='comments-approve-done'),
-
+# ROIL uses a limited set of endpoints
+#    url(r'^post/$', post_comment, name='comments-post-comment'),
+#    url(r'^posted/$', comment_done, name='comments-comment-done'),
+#    url(r'^flag/(\d+)/$', flag, name='comments-flag'),
+#    url(r'^flagged/$', flag_done, name='comments-flag-done'),
+#    url(r'^delete/(\d+)/$', delete, name='comments-delete'),
+#    url(r'^deleted/$', delete_done, name='comments-delete-done'),
+#    url(r'^approve/(\d+)/$', approve, name='comments-approve'),
+#    url(r'^approved/$', approve_done, name='comments-approve-done'),
+#
     url(r'^cr/(\d+)/(.+)/$', shortcut, name='comments-url-redirect'),
+    # removing this breaks stuff, and the endpoint is safe (just gets
+    # absolute url
 ]
